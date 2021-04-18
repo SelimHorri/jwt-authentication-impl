7d1e550c180e1f49db9d73b21ae12d465d8de34a
SelimHorri <horri.slim@gmail.com>
2021-04-18 22:43:37 +0100

    init

---
+++ src/main/java/com/selimhorri/pack/JwtAuthenticationImplApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationImplApplication.class, args);
+	}
+
+}
