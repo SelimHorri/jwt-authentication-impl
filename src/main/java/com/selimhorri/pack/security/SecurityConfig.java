62d20047c0eddd34e934bee5823dd7d3e77b86f6
SelimHorri <horri.slim@gmail.com>
2021-04-20 00:01:32 +0100

    handle authorization endpoints

--- src/main/java/com/selimhorri/pack/security/SecurityConfig.java
+++ src/main/java/com/selimhorri/pack/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.selimhorri.pack.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,7 +28,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/app/authenticate").permitAll()
+			.antMatchers("/h2-console").permitAll()
+			.anyRequest().authenticated();
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 	
 	
