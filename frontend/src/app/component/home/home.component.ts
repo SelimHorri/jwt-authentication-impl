05561ee0ee5eb7beb90001acbdd7aabd0b8ca591
SelimHorri <horri.slim@gmail.com>
2021-05-03 00:54:34 +0100

    clean up some code

--- frontend/src/app/component/home/home.component.ts
+++ frontend/src/app/component/home/home.component.ts
@@ -2,7 +2,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationRequest } from 'src/app/model/dto/authentication-request';
-import { AuthenticationResponse } from 'src/app/model/dto/authentication-response';
 import { Employee } from 'src/app/model/dto/employee';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { EmployeeService } from 'src/app/service/employee.service';
@@ -18,29 +17,16 @@ export class HomeComponent implements OnInit {
   public jwt!: string;
   public employees!: Employee[];
   
-  constructor(private authenticationService: AuthenticationService, private employeeService: EmployeeService) {
+  constructor(private employeeService: EmployeeService) {
     
   }
   
   ngOnInit(): void {
-    // this.authenticate(this.authenticationRequest);
     this.findAll(this.authenticationRequest);
   }
   
-  public authenticate(authenticationRequest: AuthenticationRequest): void {
-    this.authenticationService.authenticate(authenticationRequest).subscribe(
-      (response: AuthenticationResponse) => {
-        this.jwt = response.jwt;
-      },
-      (error: HttpErrorResponse) => {
-        console.log(error.message);
-        alert(error.message);
-      }
-    );
-  }
-  
   public findAll(authenticationRequest: AuthenticationRequest): void {
-    this.employeeService.findAll(this.authenticationRequest).subscribe(
+    this.employeeService.findAll(authenticationRequest).subscribe(
       (response: Employee[]) => {
         this.employees = response;
       },
